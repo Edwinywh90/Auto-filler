01d2ef842eea12dc96a4f95d7d25568c74eb7103
Edwin Yap <edwinedwin1990@gmail.com>
2023-05-01 21:56:53 +0800

    adapt to support portal

--- ext/script.js
+++ ext/script.js
@@ -2,18 +2,10 @@ document.getElementById("auto-fill").addEventListener("click", () => {
 	/* Auto fill form */
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		chrome.tabs.sendMessage(tabs[0].id, {
-			name: document.getElementById('name').value,
-			email: document.getElementById('email').value,
-			telephone: document.getElementById('telephone').value,
-			address1: document.getElementById('address1').value,
-			address2: document.getElementById('address2').value,
-			address3: document.getElementById('address3').value,
-			city: document.getElementById('city').value,
-			postcode: document.getElementById('postcode').value,
-			cardnumber: document.getElementById('card-number').value,
-			month: document.getElementById('month').value,
-			year: document.getElementById('year').value,
-			cvv: document.getElementById('cvv').value
+			accountname: document.getElementById('accountname').value,
+			accountlink: document.getElementById('accountlink').value,
+			description: document.getElementById('description').value,
+			email: document.getElementById('email').value
 		}, function(response) {
 			console.log(response.status);
 		});
@@ -22,33 +14,19 @@ document.getElementById("auto-fill").addEventListener("click", () => {
 
 document.getElementById("reset-fields").addEventListener("click", () => {
 	/* Reset extension form values */
-	document.getElementById('name').value = '';
+	document.getElementById('accountname').value = '';
+	document.getElementById('accountlink').value = '';
+	document.getElementById('description').value = '';
 	document.getElementById('email').value = '';
-	document.getElementById('telephone').value = '';
-	document.getElementById('address1').value = '';
-	document.getElementById('address2').value = '';
-	document.getElementById('address3').value = '';
-	document.getElementById('city').value = '';
-	document.getElementById('postcode').value = '';
-	document.getElementById('card-number').value = '';
-	document.getElementById('cvv').value = '';
 });
 
 
 document.getElementById("save").addEventListener("click", () => {
 	chrome.storage.sync.set({
-		name: document.getElementById('name').value,
-		email: document.getElementById('email').value,
-		telephone: document.getElementById('telephone').value,
-		address1: document.getElementById('address1').value,
-		address2: document.getElementById('address2').value,
-		address3: document.getElementById('address3').value,
-		city: document.getElementById('city').value,
-		postcode: document.getElementById('postcode').value,
-		cardnumber: document.getElementById('card-number').value,
-		month: document.getElementById('month').value,
-		year: document.getElementById('year').value,
-		cvv: document.getElementById('cvv').value
+		accountname: document.getElementById('accountname').value,
+		accountlink: document.getElementById('accountlink').value,
+		description: document.getElementById('description').value,
+		email: document.getElementById('email').value
 	}, function() {
 		console.log("Saved!");
 	});
@@ -56,30 +34,14 @@ document.getElementById("save").addEventListener("click", () => {
 
 document.getElementById("load").addEventListener("click", () => {
 	chrome.storage.sync.get([
-		'name',
-		'email',
-		'telephone',
-		'address1',
-		'address2',
-		'address3',
-		'city',
-		'postcode',
-		'cardnumber',
-		'month',
-		'year',
-		'cvv'
+		'accountname',
+		'accountlink',
+		'description',
+		'email'
 	], function(result) {
-		document.getElementById('name').value = result.name;
+		document.getElementById('accountname').value = result.accountname;
+		document.getElementById('accountlink').value = result.accountlink;
+		document.getElementById('description').value = result.description;
 		document.getElementById('email').value = result.email;
-		document.getElementById('telephone').value = result.telephone;
-		document.getElementById('address1').value = result.address1;
-		document.getElementById('address2').value = result.address2;
-		document.getElementById('address3').value = result.address3;
-		document.getElementById('city').value = result.city;
-		document.getElementById('postcode').value = result.postcode;
-		document.getElementById('card-number').value = result.cardnumber;
-		document.getElementById('month').value = result.month,
-		document.getElementById('year').value = result.year,
-		document.getElementById('cvv').value = result.cvv;
 	});
 });

21a7528d759c5aaaa6e0c089bb52913eebb3ce4f
Edwin Yap <edwinedwin1990@gmail.com>
2023-05-02 13:44:33 +0800

    Change auto-fill to load & fill

--- ext/script.js
+++ ext/script.js
@@ -1,4 +1,16 @@
-document.getElementById("auto-fill").addEventListener("click", () => {
+document.getElementById("load-fill").addEventListener("click", () => {
+	chrome.storage.sync.get([
+		'accountname',
+		'accountlink',
+		'description',
+		'email'
+	], function(result) {
+		document.getElementById('accountname').value = result.accountname;
+		document.getElementById('accountlink').value = result.accountlink;
+		document.getElementById('description').value = result.description;
+		document.getElementById('email').value = result.email;
+	});
+
 	/* Auto fill form */
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		chrome.tabs.sendMessage(tabs[0].id, {
@@ -11,6 +23,20 @@ document.getElementById("auto-fill").addEventListener("click", () => {
 		});
 	});
 });
+
+// document.getElementById("auto-fill").addEventListener("click", () => {
+// 	/* Auto fill form */
+// 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+// 		chrome.tabs.sendMessage(tabs[0].id, {
+// 			accountname: document.getElementById('accountname').value,
+// 			accountlink: document.getElementById('accountlink').value,
+// 			description: document.getElementById('description').value,
+// 			email: document.getElementById('email').value
+// 		}, function(response) {
+// 			console.log(response.status);
+// 		});
+// 	});
+// });
 
 document.getElementById("reset-fields").addEventListener("click", () => {
 	/* Reset extension form values */
